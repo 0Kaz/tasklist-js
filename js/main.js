76846ef25ccc4977e12faafbaf10fa96ed4b3d7b
Omar Kaz <kazeb.omar@gmail.com>
2018-09-26 18:14:58 +0100

    fixed javascript localstorage

--- js/main.js
+++ js/main.js
@@ -22,14 +22,14 @@ const getTasks = () =>{
   }
 
   tasks.forEach(function(task){
-    const span = document.createElement('span');
+    const li = document.createElement('li');
     li.className = 'collection-item';
     li.appendChild(document.createTextNode(task));
     const link = document.createElement('a');
     link.className = "delete-item secondary-content";
     link.innerHTML = '<i class="fa fa-remove"></i>';
     li.appendChild(link);
-    taskList.appendChild(span);
+    taskList.appendChild(li);
   });
 }
 
